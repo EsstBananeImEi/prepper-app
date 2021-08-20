a283a0711e22357367551abff5ec207ed139cc72
PsyOrc <sM36699736->
2021-08-20 09:44:02 +0200

    useDimension to center the spinner

--- src/components/loading-spinner/LoadingSpinner.tsx
+++ src/components/loading-spinner/LoadingSpinner.tsx
@@ -1,14 +1,15 @@
-import { Layout, Spin } from 'antd'
+import { Spin } from 'antd'
 import React, { ReactElement } from 'react'
+import { useDemensions } from '../../hooks/StorageApi'
 
 interface Props {
     message: string
 }
 
 export default function LoadingSpinner(props: Props): ReactElement {
-    const { Content } = Layout
+    const [dimensions] = useDemensions(() => 1, 0)
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: `${dimensions.height - 64}px`, minHeight: '100%' }}>
             <Spin size='large' tip={props.message}>
 
             </Spin>
