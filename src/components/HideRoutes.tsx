a1f668a95f0118415321f9421a1324858d5cd6cb
Sebastian Meine <sebastian.meine@gmx.net>
2025-06-30 10:15:02 +0200

    modify hideroutes

--- src/components/HideRoutes.tsx
+++ src/components/HideRoutes.tsx
@@ -4,9 +4,21 @@ import { useLocation } from 'react-router-dom';
 
 export default function HideRoutes() {
     const location = useLocation();
+    const allowedPaths = [
+        '/resetSuccess',
+        '/details/lebensmittel',
+        '/details/wasser',
+        '/details/medikamente',
+        '/details/hygiene',
+        '/details/informieren',
+        '/details/beduerfnisse',
+        '/details/dokumente',
+        '/details/gepaeck',
+        '/details/sicherheit',
+    ]
 
     useEffect(() => {
-        if (location.pathname !== '/resetSuccess') {
+        if (location.pathname !== '/resetSuccess' && !allowedPaths.includes(location.pathname)) {
             window.history.replaceState(null, '', '/');
         }
     }, [location]);
